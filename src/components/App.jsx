23b7c389aeee32ca7ae9ddbe9db1806c2dbcddf4
BSI <serg@example.com>
2022-02-05 11:16:34 +0300

    Установлен webpack. Выполнена первоначальная настройка.

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./app.less"
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="app">React is working! Hello, Mr. Doe...</h1>
+        </div>
+    );
+};
+
+export default App;
